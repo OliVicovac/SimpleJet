1a3fb4915485d5556949668e68f468eb911c5f49
OliVicovac <olivervicovac@hotmail.com>
2020-04-07 20:41:45 +0200

    update
    + added login
    + added firebase authentication

--- app/src/main/java/com/example/simplejet/RegisterActivity.kt
+++ app/src/main/java/com/example/simplejet/RegisterActivity.kt
@@ -1,4 +1,63 @@
 package com.example.simplejet
 
-class RegisterActivity {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_register.*
+
+class  RegisterActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        auth = FirebaseAuth.getInstance()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        btn_sign_up.setOnClickListener{
+            signUpUser()
+        }
+    }
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun signUpUser(){
+        if (text_email.text.toString().isEmpty()){
+            text_email.error="Please Enter email"
+            text_email.requestFocus()
+            return
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(text_email.text.toString()).matches()){
+            text_email.error="Please  valid email"
+            text_email.requestFocus()
+            return
+        }
+        if (text_pw.text.toString().isEmpty()){
+            text_pw.error="Please Enter password"
+            text_pw.requestFocus()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(text_email.text.toString(), text_pw.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(baseContext, "Sign Up failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+
+    }
+    fun updateUI(currentUser : FirebaseUser?){
+
+    }
 }
